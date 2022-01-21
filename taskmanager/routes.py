946163c04948e1648a3b0e56d819e246399f0b3a
will23JD <will_jd@outlook.com>
2022-01-21 13:42:13 +0000

    Able to view tasks

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -5,7 +5,8 @@ from taskmanager.models import Category, Task
 
 @app.route("/")
 def home():
-    return render_template("tasks.html")
+    tasks = list(Task.query.order_by(Task.task_name).all())
+    return render_template("tasks.html", tasks=tasks)
 
 
 @app.route("/categories")
